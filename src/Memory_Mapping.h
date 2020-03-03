4fbc502400a4be495d2f41a94226ed29769a0801
HomeMadeRobots <homemade.bots@gmail.com>
2020-03-03 16:55:27 +0100

    Add src directory, create Memory_Mapping.

---
+++ src/Memory_Mapping.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY_MAPPING_H
+#define MEMORY_MAPPING_H
+
+#if defined(__AVR__)
+	#include <avr/pgmspace.h>
+#else
+	#define PROGMEM
+	#define pgm_read_byte( byte_adrr ) (*(byte_adrr))
+#endif
+
+
+
+
+#endif
